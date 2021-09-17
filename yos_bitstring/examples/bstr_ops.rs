c301bae976fd3380f982e36a01d2c0d3dc22cf3c
Vlad Eminovici <veminovici@hotmail.com>
2021-09-17 21:36:11 +0300

    Fix the fmt issue

--- yos_bitstring/examples/bstr_ops.rs
+++ yos_bitstring/examples/bstr_ops.rs
@@ -1,4 +1,4 @@
-use yos_bitstring::{BitstringDebug, BitstringCombinators, BitstringRange};
+use yos_bitstring::{BitstringCombinators, BitstringDebug, BitstringRange};
 
 fn main() {
     let mut x = 7u8;
