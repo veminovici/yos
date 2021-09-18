4ca10b9261920c636e2d2c95f9e490dcadb0f50f
Vlad Eminovici <veminovici@hotmail.com>
2021-09-18 18:40:31 +0300

    Remove the old code

--- yos_bitstring/src/lib.rs
+++ yos_bitstring/src/lib.rs
@@ -7,17 +7,11 @@
 mod bit;
 mod bu64;
 mod bu8;
-mod primitive_u64;
-mod primitive_u8;
 mod traits;
 
 // Exposed public traits
 
 pub use crate::bit::Bit;
+pub use crate::bu64::Bu64;
 pub use crate::bu8::Bu8;
-pub use crate::primitive_u64::*;
-pub use crate::primitive_u8::*;
-pub use crate::traits::{
-    Bitstring, BitstringCombinators, BitstringConstructor, BitstringDebug, BitstringInto,
-    BitstringOps, BitstringRange, BitstringShift,
-};
+pub use crate::traits::Bitstring;
