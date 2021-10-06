97faa9c02758a662282d4bc1e0b25ca1d693762a
Vlad Eminovici <veminovici@hotmail.com>
2021-10-06 11:21:10 +0300

    Add tests for the parent and child ndx functions

--- yos-binheap/src/ndx.rs
+++ yos-binheap/src/ndx.rs
@@ -10,9 +10,64 @@ pub(super) fn parent_ndx(pos: usize, sz: usize) -> usize {
     }
 }
 
+/// Determines the index of a chlid node into a heap vector
+/// given the index of the current node, the order of the child, and the size of the nodes.
+pub(super) fn child_ndx(pos: usize, c: usize, sz: usize) -> usize {
+    pos * sz + c + 1
+}
+
 /// Determine the index of the parent for an element at a given
 /// position in a binary heap.
 #[inline]
 pub(super) fn bheap_parent_ndx(pos: usize) -> usize {
     parent_ndx(pos, 2)
+}
+
+/// Determine the index of the child for an element at a given
+/// position in a binary heap.
+#[inline]
+pub(super) fn bheap_child_ndx(pos: usize, c: usize) -> usize {
+    child_ndx(pos, c, 2)
+}
+
+#[cfg(test)]
+mod utests {
+    use super::*;
+
+    #[test]
+    fn test_parent_ndx_root() {
+        let p = parent_ndx(0, 2);
+        assert_eq!(p, 0);
+    }
+
+    #[test]
+    fn test_parent_ndx_child() {
+        assert_eq!(bheap_parent_ndx(1), 0);
+        assert_eq!(bheap_parent_ndx(2), 0);
+        assert_eq!(bheap_parent_ndx(3), 1);
+        assert_eq!(bheap_parent_ndx(4), 1);
+        assert_eq!(bheap_parent_ndx(5), 2);
+        assert_eq!(bheap_parent_ndx(6), 2);
+    }
+
+    #[test]
+    fn test_child_ndx() {
+        let c = bheap_child_ndx(0, 0);
+        assert_eq!(1, c);
+
+        let c = bheap_child_ndx(0, 1);
+        assert_eq!(2, c);
+
+        let c = bheap_child_ndx(1, 0);
+        assert_eq!(3, c);
+
+        let c = bheap_child_ndx(1, 1);
+        assert_eq!(4, c);
+
+        let c = bheap_child_ndx(2, 0);
+        assert_eq!(5, c);
+
+        let c = bheap_child_ndx(2, 1);
+        assert_eq!(6, c);
+    }
 }
