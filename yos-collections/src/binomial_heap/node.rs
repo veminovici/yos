b28f9856baf3df791e789309c7ab93bce4d0b2ea
Vlad Eminovici <veminovici@hotmail.com>
2021-10-08 18:00:28 +0300

    There might be a bug with my code.

--- yos-collections/src/binomial_heap/node.rs
+++ yos-collections/src/binomial_heap/node.rs
@@ -375,4 +375,32 @@ mod tests {
         assert_eq!(bxx.item, 10);
         assert_eq!(bxx.order, 1);
     }
+
+    #[test]
+    fn test_append_1() {
+        let mut a = Node::with_order(10, 1);
+        let a1 = Node::with_order(9, 0);
+        a.set_child(a1);
+
+        let mut b = Node::with_order(20, 1);
+        let b1 = Node::with_order(19, 0);
+        b.set_child(b1);
+
+        let mut bxa = Box::new(a);
+        let bxb = Box::new(b);
+
+        append(&mut bxa, Some(bxb));
+
+        assert_eq!(bxa.item, 20);
+        assert_eq!(bxa.order, 2);
+
+        let bxx = bxa.child.as_ref().unwrap();
+        assert_eq!(bxx.item, 10);
+        assert_eq!(bxx.order, 1);
+
+        let bxy = bxx.sibling.as_ref().unwrap();
+        assert_eq!(bxy.item, 19);
+        assert_eq!(bxy.order, 0);
+    }
+
 }
